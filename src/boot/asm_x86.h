7498b4026694214986ee4e6af6a9ac97d9a6d9c1
Shiroko <hhx.xxm@gmail.com>
2018-11-17 22:46:59 +0800

    code update

--- src/boot/asm_x86.h
+++ src/boot/asm_x86.h
@@ -2,6 +2,30 @@
 #define __OMO_ASM_X86_H__
 // 内联汇编函数，从xv6抄的
 
+static inline void insl(int port, void *addr, int cnt){
+  asm volatile("cld; rep insl"
+               :"=D"(addr), "=c"(cnt)
+               : "d" (port), "0" (addr), "1" (cnt)
+               :"memory","cc");
+}
 
+static inline void outb(unsigned short port, unsigned char data){
+  asm volatile("out %0,%1"
+               :
+               :"a"(data),"d"(port));
+}
+
+static inline unsigned char inb(unsigned short port){
+  unsigned char data;
+  asm volatile("in %1, %0":"=a"(data):"d"(port));
+  return data;
+}
+
+static inline void stosb(void *addr, int data, int cnt){
+  asm volatile("cld; rep stosb"
+               :"=D" (addr), "=c"(cnt)
+               :"0"(addr), "1"(cnt), "a"(data)
+               :"memory","cc");
+}
 
 #endif // __OMO_ASM_X86_H__
--- src/boot/boot_sector.c
+++ src/boot/boot_sector.c
@@ -3,6 +3,40 @@
 #include "asm_x86.h"
 /* #include "const.h" */
 
+typedef unsigned char u_byte;
+typedef unsigned short u_word;
+typedef unsigned int u_dword;
+typedef unsigned int uint;
+
+void waitdisk(void){
+  while((inb(0x1F7) & 0xC0) != 0x40);
+}
+
+void readsect(u_byte *addr, uint offset){
+  waitdisk();
+  outb(0x1F2, 1); // count
+  outb(0x1F3, offset);
+  outb(0x1F4, offset>>8);
+  outb(0x1F5, offset>>16);
+  outb(0x1F6, (offset >> 24) | 0xE0);
+  outb(0x1F7, 0x20); // cmd 0x20: 读取扇区
+
+  waitdisk();
+  insl(0x1F0, addr, 512 / 4);
+}
+
+void readseg(u_byte *addr, uint count, uint offset){
+  u_byte *end_addr;
+  end_addr = addr+count;
+  // 每次读取512字节（一扇区）的内容，真正地址需要向后偏移以使传入的addr指向正确的数据。
+  addr -= offset % 512;
+  // 转换字节偏移到扇区偏移
+  offset = (offset / 512) + 1;
+  for(;addr < end_addr; addr += 512, offset++)
+    readsect(addr, offset);
+}
+
+
 void boot_main(){
   /*
     MBR直接跳入保护模式，并且暴露_gdt的地址
@@ -12,5 +46,27 @@ void boot_main(){
     * 调用内核入口点，该入口点不可能返回。
     * Infos结构体的地址由memory map约定
    */
-  
+  ELF32_Header *elf;
+  ELF32_Prog *ph,*eph;
+  void (*entry)(void);
+  u_byte *addr;
+
+  elf = (ELF32_Header*)0x10000;
+
+  readseg((u_byte*)elf, 4096, 0); // 0 means 1 sector
+
+  if(elf->magic != ELF_MAGIC)
+    return;
+
+  ph = (ELF32_Prog*)((u_byte)elf + elf->phoff);
+  eph = ph + elf->phnum;
+  for(;ph<eph;ph++){
+    addr = (u_byte*)ph->paddr;
+    readseg(addr, ph->filesz, ph->off);
+    if(ph->memsz > ph->filesz) // 内存大小大于文件，补0
+      stosb(addr + ph->filesz, 0, ph->memsz - ph->filesz);
+  }
+
+  entry = (void(*)(void))(elf->entry);
+  entry();
 }
