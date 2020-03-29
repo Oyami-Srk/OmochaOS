23a407fde50da597ac17a1d1a2605df7e18db3a1
Shiroko <hhx.xxm@gmail.com>
2020-03-30 02:48:45 +0800

    allocation

--- src/kernel/header/lib/bitset.h
+++ src/kernel/header/lib/bitset.h
@@ -28,13 +28,16 @@ static inline unsigned int find_first_unset_bit(bitset *     bitarray,
         if (bitarray[i] == 0xFFFFFFFF)
             continue;
         else {
-            bitset           c                               = ~bitarray[i];
-            static const int MultiplyDeBruijnBitPosition[32] = {
-                0,  1,  28, 2,  29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4,  8,
-                31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6,  11, 5,  10, 9};
-            return MultiplyDeBruijnBitPosition
-                       [((bitset)((c & -c) * 0x077CB531U)) >> 27] +
-                   i * 32;
+            bitset       c = ~bitarray[i];
+            unsigned int r;
+            asm volatile("bsfl %0, %0" : "=a"(r) : "0"(c));
+            return r;
+            // static const int MultiplyDeBruijnBitPosition[32] = {
+            // 0,  1,  28, 2,  29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4,  8,
+            // 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6,  11, 5,  10, 9};
+            // return MultiplyDeBruijnBitPosition
+            // [((bitset)((c & -c) * 0x077CB531U)) >> 27] +
+            // i * 32;
         }
     return 0xFFFFFFFF;
 }
--- src/kernel/modules/memory.c
+++ src/kernel/modules/memory.c
@@ -45,12 +45,119 @@ struct memory_info {
 
 struct memory_info mem_info;
 
-// return 1 means need to merge
-static inline int attach_block_to_free(struct memory_info *mem,
-                                       void *block_addr, uint order) {
-    ;
-    ;
-    return 0;
+static inline uint round_down_power_2(uint x) {
+    if (x == 0)
+        return 0;
+    x |= (x >> 1);
+    x |= (x >> 2);
+    x |= (x >> 4);
+    x |= (x >> 8);
+    x |= (x >> 16);
+    return x - (x >> 1);
+}
+
+static inline uint round_up_power_2(uint x) {
+    x--;
+    x |= (x >> 1);
+    x |= (x >> 2);
+    x |= (x >> 4);
+    x |= (x >> 8);
+    x |= (x >> 16);
+    return x + 1;
+}
+
+static inline uint trailing_zero(uint x) {
+    uint c;
+    asm volatile("bsfl %0, %0" : "=a"(c) : "0"(x));
+    return c;
+}
+
+static block_list *remove_from_free_list(struct memory_info *mem, block_list *p,
+                                         uint order) {
+    if (p->prev) {
+        p->prev->next = p->next;
+        p->next->prev = p->prev;
+    } else {
+        if (mem->free_list[order] != p)
+            panic("Block has no prev but not the first.");
+        mem->free_list[order] = p->next;
+        p->next->prev         = NULL;
+    }
+    p->next = NULL;
+    p->prev = NULL;
+    return p;
+}
+
+static block_list *attach_to_free_list(struct memory_info *mem, block_list *p,
+                                       uint order) {
+    // must attach a single node
+    assert(p->prev == NULL);
+    assert(p->next == NULL);
+    p->next               = mem->free_list[order];
+    p->next->prev         = p;
+    mem->free_list[order] = p;
+    return p;
+}
+
+static char *allocate_pages(struct memory_info *mem, size_t pages) {
+    if (pages == 0)
+        return NULL;
+    uint   require_block_size = round_up_power_2(pages);
+    uint   require_order      = trailing_zero(require_block_size);
+    char * free_block         = NULL;
+    size_t free_block_size    = 0; // in pages
+    char * target_block       = NULL;
+    if (mem->free_count[require_order] == 0) {
+        for (uint o = require_order + 1; o < MAX_ORDER; o++)
+            if (mem->free_count[o] != 0) {
+                free_block =
+                    (char *)remove_from_free_list(mem, mem->free_list[o], o);
+                free_block_size = 1 << o;
+                mem->free_count[o]--;
+                uint free_page_idx =
+                    ((uint)free_block - mem->memory_start) / PG_SIZE;
+                xor_bit(mem->buddy_map[o], free_page_idx >> (o + 1), 1);
+                break;
+            }
+        return NULL;
+    } else {
+        free_block = (char *)remove_from_free_list(
+            mem, mem->free_list[require_order], require_order);
+        free_block_size = 1 << require_order;
+        mem->free_count[require_order]--;
+        uint free_page_idx = ((uint)free_block - mem->memory_start) / PG_SIZE;
+        xor_bit(mem->buddy_map[require_order],
+                free_page_idx >> (require_order + 1), 1);
+    }
+
+    target_block    = free_block + (free_block_size - pages) * PG_SIZE;
+    uint rest_pages = free_block_size - pages;
+    for (uint s = rest_pages; s > 0; s -= round_down_power_2(s)) {
+        block_list *current            = (block_list *)free_block;
+        uint        current_block_size = round_down_power_2(s);
+        uint        order              = trailing_zero(current_block_size);
+        current->prev                  = NULL;
+        if (mem->free_list[order]) {
+            block_list *next = mem->free_list[order];
+            current->next    = next;
+            next->prev       = current;
+        } else
+            current->next = NULL;
+        mem->free_list[order] = current;
+        mem->free_count[order]++;
+        free_block += (1 << order) * PG_SIZE;
+    }
+    return target_block;
+}
+
+void print_free_info(struct memory_info *mem) {
+    printf("[MEM] free block is\n[MEM] ");
+    for (uint i = 0; i < MAX_ORDER; i++)
+        printf("%04d, ", 1 << i);
+    printf("\n[MEM] ");
+    for (uint i = 0; i < MAX_ORDER; i++)
+        printf("%04d, ", mem->free_count[i]);
+    printf("\n");
 }
 
 // page_id = (page_paddr - pstart) / PG_SIZE
@@ -59,7 +166,8 @@ static inline int attach_block_to_free(struct memory_info *mem,
 // total buddy bits = ((2^MAX_ORDER)-1)*pg_count/(2^MAX_ORDER)
 
 void init_memory(struct memory_info *mem) {
-    printf("Init memory 0x%x - 0x%x\n", mem->memory_start, mem->memory_end);
+    printf("[MEM] Init memory 0x%x - 0x%x\n", mem->memory_start,
+           mem->memory_end);
     size_t pg_count = (mem->memory_end - mem->memory_start) / PG_SIZE;
     uint   buddy_total_bits =
         ((1 << (MAX_ORDER)) - 1) * pg_count / (1 << MAX_ORDER);
@@ -68,7 +176,7 @@ void init_memory(struct memory_info *mem) {
     mem->buddy_map[0]  = (bitset *)(mem->memory_end - buddy_bytes);
     mem->free_list[0]  = (block_list *)NULL;
     mem->free_count[0] = 0;
-    printf("Init buddy map on 0x%x, size %d(0x%x) bytes.\n", mem->usable_end,
+    printf("[MEM] Init buddy map on 0x%x, size %d bytes.\n", mem->usable_end,
            mem->memory_end - mem->usable_end);
     memset((void *)mem->usable_end, 0, mem->memory_end - mem->usable_end);
     for (uint i = 0; i < MAX_ORDER - 1; i++) {
@@ -93,9 +201,28 @@ void init_memory(struct memory_info *mem) {
         mem->free_list[MAX_ORDER - 1] = current;
         mem->free_count[MAX_ORDER - 1]++;
     }
+    pg -= max_block_size;
     // split reset memory into other size block
     uint rest_pages = (mem->usable_end - (uint)pg) / PG_SIZE;
-    printf("Rest of pages is %d\n", rest_pages);
+    for (uint s = rest_pages; s > 0;
+         s -= round_down_power_2(s)) { // actually those two loops can be merge
+                                       // into one, but I'm lazy to do so
+        block_list *current            = (block_list *)pg;
+        uint        current_block_size = round_down_power_2(s);
+        uint        order              = trailing_zero(current_block_size);
+        current->prev                  = NULL;
+        if (mem->free_list[order]) {
+            block_list *next = mem->free_list[order];
+            current->next    = next;
+            next->prev       = current;
+        } else
+            current->next = NULL;
+        mem->free_list[order] = current;
+        mem->free_count[order]++;
+        pg += (1 << (order)) * PG_SIZE;
+    }
+    printf("[MEM] Initialized.\n");
+    print_free_info(mem);
     return;
 }
 
@@ -113,6 +240,10 @@ void Task_Memory(void) {
     mem_info.memory_end   = core_usage.memory_end;
 
     init_memory(&mem_info);
+
+    char *test = allocate_pages(&mem_info, 89);
+    printf("Allocated 89 pages at 0x%x\n", test);
+    print_free_info(&mem_info);
 
     while (1)
         ;
