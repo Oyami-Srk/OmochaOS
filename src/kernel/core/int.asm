e07edd013d9357cc911007dbc95c3b75e717b3c1
Shiroko <hhx.xxm@gmail.com>
2020-08-23 21:03:12 +0800

    merge IVT.asm and verctor_handler.asm to int.asm, rename syscall_sysimpl.c to syscall_impl.c

--- src/kernel/core/int.asm
+++ src/kernel/core/int.asm
@@ -77,3 +77,28 @@ vector_handler_ret:
     add esp, 8 ; trapno, err pushed in IVT
     iretd
 
+;; Interrupt Vector Table
+[BITS 32]
+[SECTION .text]
+
+%assign i 0
+%rep 256
+global vector%+i
+vector%+i:
+%if (i < 10 || i > 14) && (i != 8 && i != 17)
+    push dword 0x0
+%endif
+    push dword i
+    jmp vector_handler
+%assign i i+1
+%endrep
+
+[BITS 32]
+[SECTION .data]
+global vector_table
+vector_table:
+%assign i 0
+%rep 256
+    dd vector%+i
+%assign i i+1
+%endrep
